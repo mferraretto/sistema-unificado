abb52ebd8c872d5eb51c6bfba305bb23b74edf70
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-16 13:29:20 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,24 @@
-const CACHE_NAME = 'sistema-unificado-v2';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './SOBRA_SHOPEE.html',
-  './SISTEMA_PRECIFICACAO.html',
-  './COMPARADOR_LUCRO.html',
-  './comparador.js',
-  './firebase-init.js',
-  './manifest.json',
-  './icon-512.png',
-  'https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js',
-  'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js'
-];
-
-self.addEventListener('install', (e) => {
+self.addEventListener("install", function (e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open("sistema-unificado").then(function (cache) {
+      return cache.addAll([
+        "./", // raiz
+        "./index.html",
+        "./COMPARADOR_LUCRO.html",
+        "./SISTEMA_PRECIFICACAO.html",
+        "./SOBRA_SHOPEE.html",
+        "./comparador.js",
+        "./manifest.json",
+        "./icon-512.png"
+      ]);
+    }).catch(err => console.error("Erro ao fazer cache:", err))
   );
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-});
-
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", function (e) {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
   );
 });
