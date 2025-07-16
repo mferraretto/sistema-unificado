378a4233c2c8cb085c23a299088cceea36e749c4
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-16 11:06:58 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ self.addEventListener("install", function (e) {
         "./index.html",
         "./SISTEMA_PRECIFICACAO.html",
         "./COMPARADOR_LUCRO.html",
-                "./comparador.js",
+        "./comparador.js?v=1 " ,
         "https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js",
         "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js",
         "./manifest.json",
