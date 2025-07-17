fe6dd05add83f7f0f7f532158c538433127136c2
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-16 21:08:50 -0300

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDskmCmkvGgnV9I_MkhBgHOsCoU4VarZbw",
+  authDomain: "sobra-shopee.firebaseapp.com",
+  projectId: "sobra-shopee",
+  storageBucket: "sobra-shopee.firebasestorage.app",
+  messagingSenderId: "884927523696",
+  appId: "1:884927523696:web:334f198596e85462d2bdb5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
